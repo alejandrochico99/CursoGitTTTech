b787c1e71eedfc8a0b15eb9ae018ee10341453b1
jaimeas <72698905+jaimeas@users.noreply.github.com>
2021-02-24 17:26:03 +0100

    codigo abc.c jaime

---
+++ abc.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char c;
+	
+	for(c='A';c<='Z'; c++) {
+		printf("%c",c);
+	}
+	printf("\n");
+	for(c='A'; c<='Z'; c++){
+		printf("%c%c, ",c, c+32);
+	}
+
+	printf("\n Hello World");
+	
+	printf("\n Hello Branch");	
+	return 0;
+}
