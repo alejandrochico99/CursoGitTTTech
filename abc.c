b7af44bc41cfba4ae08500af0ff8967368678f48
jaimeas <72698905+jaimeas@users.noreply.github.com>
2021-02-24 17:36:07 +0100

    codigo de jaime

---
+++ abc.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char c;
+	
+	for(c='A';c<='Z'; c++) {
+		printf("%c",c);
+	}
+	printf("\n");
+	for(c='A'; c<='Z'; c++){
+		printf("%c%c, ",c, c+32);
+	}
+
+	printf("\n Hello World");
+	printf("\n Te voy a joder el repo Alex);	
+	return 0;
+}
